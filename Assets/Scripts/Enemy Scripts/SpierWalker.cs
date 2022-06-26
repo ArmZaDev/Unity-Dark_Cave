ff0f92d036f896a632b3ca6b05c0bf18a4f0f059
ArmZaDev <arztv001@gmail.com>
2022-06-26 17:05:12 +0700

    Spider walker another way

--- Assets/Scripts/Enemy Scripts/SpierWalker.cs
+++ Assets/Scripts/Enemy Scripts/SpierWalker.cs
@@ -23,6 +23,14 @@ public class SpierWalker : MonoBehaviour
 
     private float scaleXValue;
 
+    [SerializeField]
+    private float maxWalkDistanceValue = 10f;
+
+    private float minWalkX, maxWalkX;
+
+    [SerializeField]
+    private bool walkWithGroundCheck;
+
     private void Awake()
     {
         spriteRen = GetComponent<SpriteRenderer>();
@@ -31,10 +39,15 @@ public class SpierWalker : MonoBehaviour
         moveLeft = Random.Range(0, 2) > 0 ? true : false;
 
         scaleXValue = transform.localScale.x;
+
+        minWalkX = transform.position.x - maxWalkDistanceValue;
+        maxWalkX = transform.position.x + maxWalkDistanceValue;
     }
 
     private void Update()
     {
+        HandleWalkingWithWalkDistance();
+
         HandleWalkingWithGroundCheck();
         CheckForGround();
     }
@@ -51,6 +64,11 @@ public class SpierWalker : MonoBehaviour
 
     void HandleWalkingWithGroundCheck()
     {
+        if (!walkWithGroundCheck)
+        {
+            return;
+        }
+
         tempPos = transform.position;
         tempScale = transform.localScale;
 
@@ -69,6 +87,37 @@ public class SpierWalker : MonoBehaviour
 
         transform.position = tempPos;
         transform.localScale = tempScale;
+      
+    }
+
+    void HandleWalkingWithWalkDistance()
+    {
+        if (walkWithGroundCheck)
+        {
+            return;
+        }
+
+        tempPos = transform.position;
+
+        if (moveLeft)
+        {
+            tempPos.x -= moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            tempPos.x += moveSpeed * Time.deltaTime;
+        }
+
+        transform.position = tempPos;
+
+        spriteRen.flipX = moveLeft;
+
+        if (tempPos.x < minWalkX)
+            moveLeft = false;
+
+        if (tempPos.x > maxWalkX)
+            moveLeft = true;
+
     }
 
 
