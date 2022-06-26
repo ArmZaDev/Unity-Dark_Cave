4b64b86ecdbb4c8ba7fab6d0a667d3a1517140fe
ArmZaDev <arztv001@gmail.com>
2022-06-26 18:21:47 +0700

    Spider jumper enemy

---
+++ Assets/Scripts/Enemy Scripts/SpiderJumper.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderJumper : MonoBehaviour
+{
+    private Rigidbody2D myBody;
+    private Animator anim;
+
+    [SerializeField]
+    private float minJumpForce = 5f, maxjumpForce = 12f;
+
+    [SerializeField]
+    private float minWaitTime = 1.5f, maxWailtTime = 3.5f;
+
+    private float jupmTimer;
+
+    private float canJump;
+
+
+
+
+
+
+
+}// class
+
+
+
+
+
